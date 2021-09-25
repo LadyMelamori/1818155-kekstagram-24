30532656837e3cbdba07b60d747a1fd6452ef056
LadyMelamori <boychenko.tatyana@gmail.com>
2021-09-26 02:23:27 +0300

    Функция получения случайного числа из диапазона

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function getRandomIntFromRange(from, to) {
+  if (from < 0 || to < 0) {
+    return -1;
+  }
+
+  if (to < from) {
+    return -1;
+  }
+
+  const difference = to - from;
+  const rand = Math.random();
+
+  return Math.floor(rand * (difference + 1)) + from;
+}
+
+getRandomIntFromRange(1, 3);
