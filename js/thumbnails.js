7abbf20673334279403f8faaf819203bd15349a8
LadyMelamori <boychenko.tatyana@gmail.com>
2021-11-01 19:18:42 +0300

    Правки по ревью

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,19 +1,23 @@
 import {generatePhotos, getPhotos} from './data.js';
 
-const thumbnailList = document.querySelector('.pictures');
-const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const addThumbnails = () => {
+  const thumbnailListElement = document.querySelector('.pictures');
+  const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-generatePhotos();
-const photos = getPhotos();
+  generatePhotos();
+  const photos = getPhotos();
 
-const thumbnailsFragment = document.createDocumentFragment();
+  const thumbnailsFragment = document.createDocumentFragment();
 
-photos.forEach((photo) => {
-  const thumbnail = thumbnailTemplate.cloneNode(true);
-  thumbnail.querySelector('.picture__img').src = photo.url;
-  thumbnail.querySelector('.picture__likes').textContent = photo.likes;
-  thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
-  thumbnailsFragment.appendChild(thumbnail);
-});
+  photos.forEach((photo) => {
+    const thumbnail = thumbnailTemplate.cloneNode(true);
+    thumbnail.querySelector('.picture__img').src = photo.url;
+    thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+    thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+    thumbnailsFragment.appendChild(thumbnail);
+  });
 
-thumbnailList.appendChild(thumbnailsFragment);
+  thumbnailListElement.appendChild(thumbnailsFragment);
+};
+
+addThumbnails();
