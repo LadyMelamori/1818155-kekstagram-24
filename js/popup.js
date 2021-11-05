10ccc5c07ece80fe09bbc9db2046b1d1b5f4df05
LadyMelamori <boychenko.tatyana@gmail.com>
2021-11-05 05:23:41 +0300

    Показываем картинку полностью в попапе

---
+++ js/popup.js
@@ -0,0 +1,74 @@
+import {isEscapeKey} from './utils.js';
+
+const popupElement = document.querySelector('.big-picture');
+const popupImgElement = popupElement.querySelector('.big-picture__img img');
+const popupLikesCountElement = popupElement.querySelector('.likes-count');
+const popupCommentsCountElement = popupElement.querySelector('.comments-count');
+const popupCommentsListElement = popupElement.querySelector('.social__comments');
+const popupCaptionElement = popupElement.querySelector('.social__caption');
+const popupShownCommentsCountElement = popupElement.querySelector('.social__comment-count');
+const popupCommentsLoaderElement = popupElement.querySelector('.comments-loader');
+const popupCloseButton = popupElement.querySelector('.big-picture__cancel');
+const bodyElement = document.querySelector('body');
+
+const getCommentMarkup = (comment) => {
+  const commentElement = document.createElement('li');
+  commentElement.classList.add('social__comment');
+
+  const commentAvatar = document.createElement('img');
+  commentAvatar.classList.add('social__picture');
+  commentAvatar.src = comment.avatar;
+  commentAvatar.alt = comment.name;
+  commentAvatar.width = 35;
+  commentAvatar.height = 35;
+  commentElement.appendChild(commentAvatar);
+
+  const commentText = document.createElement('p');
+  commentText.classList.add('social__text');
+  commentText.textContent = comment.message;
+  commentElement.appendChild(commentText);
+
+  return commentElement;
+};
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const showPopup = (photo) => {
+  popupElement.classList.remove('hidden');
+
+  popupImgElement.src = photo.url;
+  popupLikesCountElement.textContent = photo.likes;
+  popupCommentsCountElement.textContent = photo.comments.length;
+
+  const commentsMarkup = photo.comments.map((x) => getCommentMarkup(x));
+  const commentsFragment = document.createDocumentFragment();
+  commentsMarkup.forEach((comment) => commentsFragment.appendChild(comment));
+  popupCommentsListElement.innerHTML = '';
+  popupCommentsListElement.appendChild(commentsFragment);
+
+  popupCaptionElement.textContent = photo.description;
+
+  popupShownCommentsCountElement.classList.add('hidden');
+  popupCommentsLoaderElement.classList.add('hidden');
+
+  bodyElement.classList.add('modal-open');
+
+  document.addEventListener('keydown', onPopupEscKeydown);
+};
+
+const closePopup = () => {
+  popupElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
+popupCloseButton.addEventListener('click', () => {
+  closePopup();
+});
+
+export {showPopup};
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,4 +1,5 @@
 import {generatePhotos, getPhotos} from './data.js';
+import {showPopup} from './popup.js';
 
 const addThumbnails = () => {
   const thumbnailListElement = document.querySelector('.pictures');
@@ -14,10 +15,21 @@ const addThumbnails = () => {
     thumbnail.querySelector('.picture__img').src = photo.url;
     thumbnail.querySelector('.picture__likes').textContent = photo.likes;
     thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+    thumbnail.dataset.photoId = photo.id;
     thumbnailsFragment.appendChild(thumbnail);
   });
 
   thumbnailListElement.appendChild(thumbnailsFragment);
+
+  thumbnailListElement.addEventListener('click', (evt) => {
+    const picture = evt.target.closest('.picture');
+    if (picture) {
+      evt.preventDefault();
+      const photoId = parseInt(picture.dataset.photoId, 10);
+      const photo = photos.find((x) => x.id === photoId);
+      showPopup(photo);
+    }
+  });
 };
 
 addThumbnails();
