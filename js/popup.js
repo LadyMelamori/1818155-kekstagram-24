0e4e40e6b02798f1cf541b0e2b88780842b3779c
LadyMelamori <boychenko.tatyana@gmail.com>
2021-11-08 01:32:03 +0300

    Подгружаем комменты по 5 штук

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,14 @@
 import {isEscapeKey} from './utils.js';
 
+const COMMENTS_BATCH_SIZE = 5;
+
 const popupElement = document.querySelector('.big-picture');
 const popupImgElement = popupElement.querySelector('.big-picture__img img');
 const popupLikesCountElement = popupElement.querySelector('.likes-count');
 const popupCommentsCountElement = popupElement.querySelector('.comments-count');
 const popupCommentsListElement = popupElement.querySelector('.social__comments');
 const popupCaptionElement = popupElement.querySelector('.social__caption');
-const popupShownCommentsCountElement = popupElement.querySelector('.social__comment-count');
+const popupShownCommentsCountElement = popupElement.querySelector('.shown-comments-count');
 const popupCommentsLoaderElement = popupElement.querySelector('.comments-loader');
 const popupCloseButtonElement = popupElement.querySelector('.big-picture__cancel');
 const bodyElement = document.querySelector('body');
@@ -39,6 +41,28 @@ const onPopupEscKeydown = (evt) => {
   }
 };
 
+const generateShowNextCommentsBatchFunction = (comments) => {
+  let shownCommentsCount = 0;
+
+  const showNextCommentsBatchFunction = () => {
+    const commentsBatch = comments.slice(shownCommentsCount, shownCommentsCount + COMMENTS_BATCH_SIZE);
+    const commentsMarkup = commentsBatch.map((x) => getCommentMarkup(x));
+    const commentsFragment = document.createDocumentFragment();
+    commentsMarkup.forEach((comment) => commentsFragment.appendChild(comment));
+    popupCommentsListElement.appendChild(commentsFragment);
+    shownCommentsCount += commentsBatch.length;
+
+    popupShownCommentsCountElement.textContent = shownCommentsCount;
+    if (comments.length === shownCommentsCount) {
+      popupCommentsLoaderElement.classList.add('hidden');
+    }
+  };
+
+  return showNextCommentsBatchFunction;
+};
+
+let showNextCommentsBatch = () => {};
+
 const showPopup = (photo) => {
   popupElement.classList.remove('hidden');
 
@@ -46,16 +70,13 @@ const showPopup = (photo) => {
   popupLikesCountElement.textContent = photo.likes;
   popupCommentsCountElement.textContent = photo.comments.length;
 
-  const commentsMarkup = photo.comments.map((x) => getCommentMarkup(x));
-  const commentsFragment = document.createDocumentFragment();
-  commentsMarkup.forEach((comment) => commentsFragment.appendChild(comment));
   popupCommentsListElement.innerHTML = '';
-  popupCommentsListElement.appendChild(commentsFragment);
+  popupCommentsLoaderElement.classList.remove('hidden');
+  showNextCommentsBatch = generateShowNextCommentsBatchFunction(photo.comments);
+  showNextCommentsBatch();
+  popupCommentsLoaderElement.addEventListener('click', showNextCommentsBatch);
 
   popupCaptionElement.textContent = photo.description;
-
-  popupShownCommentsCountElement.classList.add('hidden');
-  popupCommentsLoaderElement.classList.add('hidden');
 
   bodyElement.classList.add('modal-open');
 
@@ -66,6 +87,7 @@ const closePopup = () => {
   popupElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
+  popupCommentsLoaderElement.removeEventListener('click', showNextCommentsBatch);
 };
 
 popupCloseButtonElement.addEventListener('click', closePopup);
